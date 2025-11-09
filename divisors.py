21ae86ece4f50278df0612eed6493498a0ad112e
deby06-wq <deby06@gachon.ac.kr>
2025-11-09 21:43:31 +0900

    Add function to count divisors of a number

--- divisors.py
+++ divisors.py
@@ -1,1 +1,13 @@
+import sys
+
+def count_divisors(n: int) -> int:
+    cnt = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            cnt += 1
+    return cnt
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    print(count_divisors(n))
 
